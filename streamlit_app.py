17d67477d420295741976973cf17e39d92a487c2
alvinpalacios-cmyk <alvin.palacios@maricopa.gov>
2025-10-07 11:22:39 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import request
+import requests
 
 cnx = st.connection("snowflake")
 session = cnx.session()
